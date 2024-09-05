86b38243ad3305c4e4b0ae7eded3bc23f9fd3930
ANJAN <devopsanjan700@gmail.com>
2024-09-05 14:20:22 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+exports.handler = async (event) => {
+    console.log("Request event:", event); // Optional: Log the incoming event for debugging
+    
+    return {
+        statusCode: 200,
+        headers: {
+            "Content-Type": "text/plain"
+        },
+        body: "Hello World"
+    };
+};
